7dff642c20b18d21d2dd1798f9716ee1ac966cfe
Harold Jean <haroldjean38@yahoo.com>
2022-11-08 20:34:25 -0500

    added display

--- app.js
+++ app.js
@@ -5,48 +5,78 @@ import { renderGoblin, renderHealth } from './render-utils.js';
 const healthEl = document.querySelector('health');
 const goblinEl = document.querySelector('goblins-stats');
 const goblinInput = document.querySelector('goblin-input');
-const addGoblinButton = document.getElementById('goblin-button')
+const addGoblinButton = document.getElementById('goblin-button');
 const goblinForm = document.querySelector('form');
 
 /* State */
-
+let GoblinData = 0;
 let healthCount = 10;
-
+let goblinCount = 10;
 const goblinData = [
-    {
-        name: 'Red Goblin',
-        satisfaction: 5,
-    },
-    {
-        name: 'Yellow Goblin',
-        satisfaction: 5,
-    },
+    { id: 1, name: 'Mean One', hp: 5 },
+    { id: 2, name: 'Big Bad One', hp: 5 },
 ];
 
 /* Events */
-addGoblinButton.addEventListener('click', () => {
-    if (Math.random() > .7) {
+goblinInput.addEventListener('click', () => {
+    if (Math.random() > 0.7) {
         alert('You hit the goblin!');
+    } else if (Math.random() > 0.5) {
+        alert('You missed! No damage');
+    } else {
+        alert('You missed! You took damage');
     }
-    else {
-        alert('You missed! No damage')
-    }
-    else if {
-        alert('You missed! You took damage')
-    }
-})
+});
 /* Display Functions */
 function displayGoblins() {
     goblinEl.textContent = '';
     for (let goblin of goblinData) {
-        const goblinEl = renderGoblin(goblins);
+        const goblinEl = renderGoblin(goblin);
         goblinEl.addEventListener('click', () => {
-            if (goblins.satisfaction < 2 && goblinCount > 0) {
-                goblins.satisfaction++;
+            if (goblin.satisfaction < 2 && goblinCount > 0) {
+                goblin.satisfaction++;
+                displayGoblins();
             }
-        })
+        });
     }
 }
+//add new names
+addGoblinButton.addEventListener('submit', (e) => {
+    // prevent default behavior of a form submit
+    e.preventDefault();
+
+    const data = new GoblinData(goblinEl);
+
+    //make a new goblin object with the user input
+    const goblinId = {
+        id: goblinData,
+        name: data.get('goblin-name'),
+        hp: Math.ceil(Math.random() * 5),
+    };
+
+    goblinId++;
+
+    //add newgoblin object to the array of goblins in state
+    goblins.push(newGoblin);
+
+    displaygoblins();
+});
+
+/* Display Functions */
+
+// function displaygoblins() {
+//     goblinEl.textContent = '';
+
+//     for (let goblin of goblins) {
+//         const goblinEl = renderGoblin(goblin);
+//         goblinEl.addEventListener('click', () => {
+//             goblinClickHandler(goblin);
+//         });
+//         goblinListEl.append(goblinEl);
+//     }
+// }
+
+displayGoblins();
 // (don't forget to call any display functions you want to run on page load!)
 displayGoblinStats();
 displayHeath();
